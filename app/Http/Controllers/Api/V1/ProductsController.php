0ac3ab1631b3ddb0f22265dfd045ce841f712cae
PauloRobert <pauloblacksheep@hotmail.com>
2018-10-25 17:22:07 -0300

    adicionando rotas e controller de produto

---
+++ app/Http/Controllers/Api/V1/ProductsController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Leroy\Http\Controllers\Api\V1;
+
+use Illuminate\Http\Request;
+use Leroy\Http\Controllers\Controller;
+
+class ProductsController extends Controller
+{
+    //
+}
--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,20 @@
 <?php
 
-use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware'=>'cors'], function (){
+    
+    Route::prefix('v1')
+        ->namespace('Api\V1')
+        ->group(function () {
+        /**
+         * Produtos
+        */
+        Route::group(['prefix' => 'procudts'], function () {
+            Route::post('/import','ProductsController@import');
+            
+        });
+        Route::resource('products', 'ProductsController',['except' => ['edit', 'create','show']]);
+            
+        
+        });
 });
