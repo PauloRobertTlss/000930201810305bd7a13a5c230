a23640f6edd1330471cee0cc174c4e4ee535eb3f
PauloRobert <pauloblacksheep@hotmail.com>
2018-10-26 17:54:03 -0300

    refatorando metodo, atualizando progresso do model documento

--- app/Jobs/RegisterProductsInBackgroud.php
+++ app/Jobs/RegisterProductsInBackgroud.php
@@ -15,6 +15,7 @@ class RegisterProductsInBackgroud implements ShouldQueue
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
     public $doc;
+    private $products;
     /**
      * Create a new job instance.
      *
@@ -35,33 +36,41 @@ class RegisterProductsInBackgroud implements ShouldQueue
     {
         
          try{
-           /**
-           * Gerar uma coleção: Como regra de Negócio(esboço) a planilha será carregada apenas uma vez e depois descartada. O Job aguarda um @array.
-           */
-            $collection = (new BotExcel)->import($this->path_file_temp);
+           
+           $collections = (new BotExcel)->import($this->doc->path);
+           $this->products = $collections->toArray();
+           unset($collections);
+               $this->doc->processed = true;
+               $this->doc->progress = 1;
+               $this->doc->save();
+           
          } catch (\Box\Spout\Common\Exception\SpoutException $e){
-               unlink($tmpFile);
-               return response()->json(['error'=>true,'message'=>'Planilha com erros'],422);
+               unlink($this->doc->path);
+               $this->doc->processed = true;
+               $this->doc->progress = 2;
+               $this->doc->save();
          }
          
-        \Log::info("cadatro de produtos em background");
-        $repository = app(\Leroy\Repositories\Interfaces\ProductRepository::class);
-        $repositoryCategory = app(\Leroy\Repositories\Interfaces\CategoryRepository::class);
-        $repository->skipPresenter(true);
-        $repositoryCategory->skipPresenter(true);
-        $checkCategory=false;
-        
-        $first_product = array_first($this->products);
-        if(isset($first_product['category_id'])){
-            $category = $repositoryCategory->findByField('id',$first_product['category_id'])->first();
-            $checkCategory = !empty($category);
-            unset($category);
-        }
-        
-        unset($first_product);
-            foreach ($this->products as $p){
-                $p = $checkCategory ? $p : array_except($p,'category_id');
-                $repository->create($p);
+         if(count($this->products)){
+            \Log::info("cadatro de produtos em background");
+            $repository = app(\Leroy\Repositories\Interfaces\ProductRepository::class);
+            $repositoryCategory = app(\Leroy\Repositories\Interfaces\CategoryRepository::class);
+            $repository->skipPresenter(true);
+            $repositoryCategory->skipPresenter(true);
+            $checkCategory=false;
+
+            $first_product = array_first($this->products);
+            if(isset($first_product['category_id'])){
+                $category = $repositoryCategory->findByField('id',$first_product['category_id'])->first();
+                $checkCategory = !empty($category);
+                unset($category);
             }
+
+            unset($first_product);
+                foreach ($this->products as $p){
+                    $p = $checkCategory ? $p : array_except($p,'category_id');
+                    $repository->create($p);
+                }
+        }
     }
 }
