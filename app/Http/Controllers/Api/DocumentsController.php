779f8bffa8abf95ed7ee5c86fd6c3b101dcff873
PauloRobert <pauloblacksheep@hotmail.com>
2018-10-26 18:28:36 -0300

    adicinando metodos para listagem dos processos PENDENTES or ENCERRADO

--- app/Http/Controllers/Api/DocumentsController.php
+++ app/Http/Controllers/Api/DocumentsController.php
@@ -5,7 +5,7 @@ namespace Leroy\Http\Controllers\Api;
 use Illuminate\Http\Request;
 use Leroy\Http\Controllers\Controller;
 use Leroy\Repositories\Interfaces\DocumentRepository;
-
+use Leroy\Criteria\DocumentInProcessedCriteria;
 class DocumentsController extends Controller
 {
     /**
@@ -20,9 +20,20 @@ class DocumentsController extends Controller
        $this->repository = $repository;
     }
     
-    public function processed(string $hash)
+    public function show(string $hash)
     {
         return $this->repository->findByField('hash_endpoing', $hash);   
+    }
+    
+    public function pendents()
+    {
+        $this->repository->pushCriteria(new DocumentInProcessedCriteria(false));
+        return $this->repository->all();
+    }
+    public function processed()
+    {
+        $this->repository->pushCriteria(new DocumentInProcessedCriteria(true));
+        return $this->repository->all();   
     }
     
 }
