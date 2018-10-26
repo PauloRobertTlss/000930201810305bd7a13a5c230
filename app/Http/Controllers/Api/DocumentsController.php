62daa3eab7332a7a7bde36bbd5a46cd9467d0e1c
PauloRobert <pauloblacksheep@hotmail.com>
2018-10-26 17:00:52 -0300

    controle para endpoint

---
+++ app/Http/Controllers/Api/DocumentsController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Leroy\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use Leroy\Http\Controllers\Controller;
+use Leroy\Repositories\Interfaces\DocumentRepository;
+
+class DocumentsController extends Controller
+{
+    /**
+     *
+     * @var type 
+     */
+    private $repository;
+    private $services;
+    
+    public function __construct(DocumentRepository $repository)
+    {
+       $this->repository = $repository;
+    }
+    
+    public function processed(string $hash){
+        dd($hash);
+    }
+    
+}
