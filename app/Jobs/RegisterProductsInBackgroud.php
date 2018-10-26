9dd6e030a61b27ac1eb2446465f9d75d5a38b840
PauloRobert <pauloblacksheep@hotmail.com>
2018-10-26 16:07:26 -0300

    adicionando Documento ao contrutor

--- app/Jobs/RegisterProductsInBackgroud.php
+++ app/Jobs/RegisterProductsInBackgroud.php
@@ -7,20 +7,22 @@ use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
+use Leroy\Excel\Bot\Bot as BotExcel;
+use Leroy\Entities\Document;
 
 class RegisterProductsInBackgroud implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-    public $path_file_temp;
+    public $doc;
     /**
      * Create a new job instance.
      *
      * @return void
      */
-    public function __construct(string $path)
+    public function __construct(Document $doc)
     {
-        $this->path_file_temp = $path;
+        $this->doc = $doc;
     }
 
     /**
@@ -31,6 +33,17 @@ class RegisterProductsInBackgroud implements ShouldQueue
     
     public function handle()
     {
+        
+         try{
+           /**
+           * Gerar uma coleção: Como regra de Negócio(esboço) a planilha será carregada apenas uma vez e depois descartada. O Job aguarda um @array.
+           */
+            $collection = (new BotExcel)->import($this->path_file_temp);
+         } catch (\Box\Spout\Common\Exception\SpoutException $e){
+               unlink($tmpFile);
+               return response()->json(['error'=>true,'message'=>'Planilha com erros'],422);
+         }
+         
         \Log::info("cadatro de produtos em background");
         $repository = app(\Leroy\Repositories\Interfaces\ProductRepository::class);
         $repositoryCategory = app(\Leroy\Repositories\Interfaces\CategoryRepository::class);
