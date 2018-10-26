5a19fc7634144e95ce297607d452e1d53f768273
PauloRobert <pauloblacksheep@hotmail.com>
2018-10-26 17:11:34 -0300

    adicionado metodo para tempo decorrido após processamento

--- app/Entities/Document.php
+++ app/Entities/Document.php
@@ -2,6 +2,7 @@
 
 namespace Leroy\Entities;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 use Prettus\Repository\Contracts\Transformable;
 use Prettus\Repository\Traits\TransformableTrait;
@@ -22,4 +23,38 @@ class Document extends Model implements Transformable
      */
     protected $fillable = ['id','name','path','file_display','hash_endpoing','processed'];
 
+    
+    public function time_elapsed_string($full = false)
+    {
+
+        $now = Carbon::now('America/Sao_Paulo'); //Your timezone
+        $ago = Carbon::createFromTimeStamp(strtotime($this->updated_at));
+
+        $diff = $now->diff(new \DateTime($ago));
+
+        //return $diff;
+
+        $diff->w = floor($diff->d / 7);
+        $diff->d -= $diff->w * 7;
+
+        $string = [
+            'y' => 'ano',
+            'm' => 'mês',
+            'w' => 'semana',
+            'd' => 'dia',
+            'h' => 'hora',
+            'i' => 'minuto',
+            's' => 'segundo',
+        ];
+        foreach ($string as $k => &$v) {
+            if ($diff->$k) {
+                $v = $diff->$k . ' ' . $v . ($diff->$k > 1 ? 's' : '');
+            } else {
+                unset($string[$k]);
+            }
+        }
+
+        if (!$full) $string = array_slice($string, 0, 1);
+        return $string ? implode(', ', $string) . ' atrás' : 'agora mesmo';
+    }
 }
