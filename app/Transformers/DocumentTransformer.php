d972fc29f506803ad65b4e3c87fd4f08550484ed
PauloRobert <pauloblacksheep@hotmail.com>
2018-10-26 18:03:30 -0300

    renomando metodo

--- app/Transformers/DocumentTransformer.php
+++ app/Transformers/DocumentTransformer.php
@@ -29,7 +29,7 @@ class DocumentTransformer extends TransformerAbstract
         ];
         
         if($doc->processed){
-            $result['comment'] = $this->timeElapsedString();
+            $result['comment'] = $doc->timeElapsedString();
         }
         
         return $result;
