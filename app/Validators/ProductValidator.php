214a593561cf54b5bffe91c810d4bdd8b20bcc6f
PauloRobert <pauloblacksheep@hotmail.com>
2018-10-28 12:22:54 -0300

    validatores

--- app/Validators/ProductValidator.php
+++ app/Validators/ProductValidator.php
@@ -14,10 +14,11 @@ class ProductValidator extends LaravelValidator
 {
     
     protected $messages = [
+        'lm.required' => 'O código único lm* é obrigatório',
         'name.required' => 'O name é obrigatório',
         'description.required' => 'Uma descrição é obrigatório',
         'price.required' => 'O preço é obrigatório',
-        'name.min' => 'minímo para name 10 caracteres'
+        'name.min' => 'minímo para name 5 caracteres'
     ];
     /**
      * Validation Rules
@@ -26,14 +27,12 @@ class ProductValidator extends LaravelValidator
      */
     protected $rules = [
         ValidatorInterface::RULE_CREATE => [
+            'lm' => 'required|unique:products',
             'name' => 'required|min:5',
             'price'  => 'required',
             'description'=> 'required'
         ],
         ValidatorInterface::RULE_UPDATE => [
-            'name' => 'required|min:10',
-            'price'  => 'required',
-            'description'=> 'required'
         ],
     ];
 }
