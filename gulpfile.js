0679ae81d1d54e20f9b019c42263413e8013b38b
PauloRobert <pauloblacksheep@hotmail.com>
2018-10-29 18:47:23 -0300

    removendo tarefa html

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,11 @@
 const gulp = require('gulp');
 const apidoc = require('gulp-api-doc');
-const htmltidy = require('gulp-htmltidy');
+
 
 require('laravel-elixir-apidoc');
 
-gulp.task('doc', (done) => {
-    return gulp.src('project/documentation/api/**/*.php')
-        .pipe(apidoc({
-                  dest: "build/",
-                  template: "documentation/",
-                  debug: true
-              },done))
+gulp.task('doc', () => {
+    return gulp.src('tests/Feature/*.php')
+        .pipe(apidoc())
         .pipe(gulp.dest('documentation'));
-});
-
-gulp.task('html', function() {
-  return gulp.src('./documentation/*.html')
-        .pipe(htmltidy({doctype: 'html5',
-                       hideComments: true,
-                       indent: false}))
-        .pipe(gulp.dest('build/'));;
 });
