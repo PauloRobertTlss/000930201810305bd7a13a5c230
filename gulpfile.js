3c0ee6bb8af398f3ff2d38d2b9c7784c8d1a851c
PauloRobert <pauloblacksheep@hotmail.com>
2018-10-29 09:59:50 -0300

    adicionando gulp-doc

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,23 @@
 const gulp = require('gulp');
 const apidoc = require('gulp-api-doc');
+const htmltidy = require('gulp-htmltidy');
+
 require('laravel-elixir-apidoc');
 
-gulp.task('doc', () => {
+gulp.task('doc', (done) => {
     return gulp.src('project/documentation/api/**/*.php')
-        .pipe(apidoc())
+        .pipe(apidoc({
+                  dest: "build/",
+                  template: "documentation/",
+                  debug: true
+              },done))
         .pipe(gulp.dest('documentation'));
+});
+
+gulp.task('html', function() {
+  return gulp.src('./documentation/*.html')
+        .pipe(htmltidy({doctype: 'html5',
+                       hideComments: true,
+                       indent: false}))
+        .pipe(gulp.dest('build/'));;
 });
