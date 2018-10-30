b6f4929095152e2e9b15128dc205635bf486eb63
PauloRobert <pauloblacksheep@hotmail.com>
2018-10-30 12:58:20 -0300

    remover elixir-doc

--- gulpfile.js
+++ gulpfile.js
@@ -9,3 +9,9 @@ gulp.task('doc', () => {
         .pipe(apidoc())
         .pipe(gulp.dest('documentation'));
 });
+
+gulp.task('doc2', function(){
+    return gulp.src('tests/Feature/*.php')
+        .pipe(apidoc())
+        .pipe(gulp.dest('documentation'));
+});
